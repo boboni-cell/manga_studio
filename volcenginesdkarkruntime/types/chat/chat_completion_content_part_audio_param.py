4737c69f47c2989e3600d2cea6f04a204159cc6d
zhyyyy0706@gmail.com <zhyyyy0706@gmail.com>
2026-06-14 19:00:32 +0900

    vendor volcenginesdkarkruntime for Railway deploy

---
+++ volcenginesdkarkruntime/types/chat/chat_completion_content_part_audio_param.py
@@ -0,0 +1,33 @@
+# Copyright (c) [2025] [OpenAI]
+# Copyright (c) [2025] [ByteDance Ltd. and/or its affiliates.]
+# SPDX-License-Identifier: Apache-2.0
+#
+# This file has been modified by [ByteDance Ltd. and/or its affiliates.] on 2025.7
+#
+# Original file was released under Apache License Version 2.0, with the full license text
+# available at https://github.com/openai/openai-python/blob/main/LICENSE.
+#
+# This modified file is released under the same license.
+
+from __future__ import annotations
+
+from typing_extensions import Literal, Required, TypedDict
+from typing import Optional
+
+__all__ = ["ChatCompletionContentPartAudioParam", "InputAudio"]
+
+
+class InputAudio(TypedDict, total=False):
+    url: Optional[str]
+    """Either a URL of the audio."""
+    data: Optional[str]
+    """Base64 encoded audio data."""
+    format: Optional[str]
+    """audio encoding format"""
+
+
+class ChatCompletionContentPartAudioParam(TypedDict, total=False):
+    input_audio: Required[InputAudio]
+
+    type: Required[Literal["input_audio"]]
+    """The type of the content part."""
